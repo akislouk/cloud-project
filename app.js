e19829e344819c71bd85f9b401968f55b54a9423
akislouk <spyrosloukatos@gmail.com>
2022-11-28 10:08:04 +0200

    remove cart flash

--- app.js
+++ app.js
@@ -80,7 +80,6 @@ app.use((req, res, next) => {
     res.locals.user = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.cart = req.flash("cart");
     next();
 });
 
