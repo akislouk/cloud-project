30a063091d296e4cf59808bef12e4e1fb25002a6
akislouk <spyrosloukatos@gmail.com>
2022-11-28 18:21:16 +0200

    install Joi and sanitize-html

--- models/db.js
+++ models/db.js
@@ -19,7 +19,7 @@ export function init() {
         host: process.env.DB_HOST || "localhost",
         port: process.env.DB_PORT || 3306,
         user: process.env.DB_USER || "root",
-        password: process.env.DB_PASS || "akis11",
+        password: process.env.DB_PASS || "root",
         multipleStatements: true,
     });
     connection.connect();
